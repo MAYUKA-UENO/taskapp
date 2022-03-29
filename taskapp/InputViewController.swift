833767c598bc08bfd373324375272ff6a190a34a
MAYUKA-UENO <wishwishjanuary@yahoo.co.jp>
2022-03-29 23:03:11 +0900

    pod installとgitignoreの作成まで実施

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,22 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by まゆか on 2022/03/29.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -1,19 +1,44 @@
-//
-//  ViewController.swift
-//  taskapp
-//
-//  Created by まゆか on 2022/03/27.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.fillerRowHeight = UITableView.automaticDimension//データを表示していない部分に罫線を表示する
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    //データの数を返すメソッド
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)-> Int
+    {
+        return 0
+    }
+    
+    //各セルの内容を返すメソッド
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)//再利用可能なcellを得る
+        return cell
     }
 
+    //各セルを選択した時に実行されるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        performSegue(withIdentifier: "cellSegue",sender: nil)
+    }
 
+    //各セルが削除が可能なことを伝えるメソッド(削除可能かどうか、並び替えが可能かどうかなど、編集ができるかを返す部分)
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle
+    {
+        return .delete
+    }
+
+    //Deleteボタンが押された時に呼ばれるメソッド
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {}
 }
 
